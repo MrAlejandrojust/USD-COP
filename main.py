6e042a53a290167ba233921a68e5466c331c58a6
Alejandro Osorio <al3jandrojust@gmail.com>
2022-03-03 23:26:59 -0500

    Cleaned Again

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import yfinance as yf
 
+import time
 
 from os import system
 
 
 def main():
-    try:
-        while True:
-            # print('\nPrice is currently loading please wait...')
-            usdcop = yf.download('USDCOP=X', period='1d')['Open']
+    while True:
+        # print('\nPrice is currently loading please wait...')
+        usdcop = yf.download('USDCOP=X', period='1d')['Open']
 
-            for k, v in usdcop.items():
-                v = "{0:.2f}".format(v)
-                print('\nCurrent Price:', v)
+        for k, v in usdcop.items():
+            v = "{0:.2f}".format(v)
+            print('\nCurrent Price:', v)
 
-            time.sleep(10)
-            system("cls")
-    except:
-        print('\nSuccessfully stopped!')
+        time.sleep(10)
+        system("cls")
 
 
 if __name__ == "__main__":
