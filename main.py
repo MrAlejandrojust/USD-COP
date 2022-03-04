673bdbc2eee6f32fd49a45d242e0b799eedc3264
Alejandro Osorio <al3jandrojust@gmail.com>
2022-03-04 00:51:53 -0500

    JustTrade-0.4

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import yfinance as yf
 
+from bs4 import BeautifulSoup
+import requests
+
 import time
 
 from os import system
 
 
 def main():
+    # Today TRM
+    URL = 'https://www.trmhoy.co/en/'
+    req = requests.get(URL)
+
+    status_code = req.status_code
+
+    if status_code == 200:
+        html = BeautifulSoup(req.text, "html.parser")
+        trm_class = html.find('div', {'class': 'col-4 col-12-medium'})
+        trm_today = trm_class.find('h2').getText()
+
+    # Real Time TRM
     while True:
-        # print('\nPrice is currently loading please wait...')
         usdcop = yf.download('USDCOP=X', period='1d')['Open']
 
         for k, v in usdcop.items():
             v = "{0:.2f}".format(v)
-            print('\nCurrent Price:', v)
+            print('\nRT Price: $' + v)
 
-        time.sleep(10)
-        system("cls")
+        print('TRM Today:', trm_today)
+
+        try:
+            time.sleep(10)
+            system("cls")
+        except:
+            system("cls")
 
 
 if __name__ == "__main__":
